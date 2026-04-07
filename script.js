b5700af286d3563ddf2e452d714f97df803443d9
nicole <nicole_b_silva@estudante.sesisenai.org.br>
2026-04-07 12:27:35 -0300

    add mais coisas

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function formulario () {
+    let nome= document.getElementById("nome").value;
+    let email= document.getElementById("email").value;
+    let telefone= document.getElementById("telefone").value;
+    let cpf= document.getElementById("cpf").value;
+    let idade= document.getElementById("idade").value;
+    let cidade= document.getElementById("cidade").value;
+    let moradia= document.getElementById("moradia").value;
+    let quintal= document.getElementById("quintal").value;
+    let pet= document.getElementById("pet").value;
+    let horas= document.getElementById("horas").value;
+    let motivo= document.getElementById("motivo").value;
     
+    if(nome.length < 3) {
+        alert("O nome deve ter no mínimo 3 caracteres");
+    }
 }
